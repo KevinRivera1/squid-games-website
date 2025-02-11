a2c62d3d3bfcdf8131bfdded57220496a602fc06
Kevin Rivera <shimpo11.kr@gmail.com>
2025-02-11 15:23:23 -0500

    Agregar datos de participantes para el componente Card

---
+++ src/data/card/card.data.ts
@@ -0,0 +1,23 @@
+interface ICardItems {
+  image: string;
+  name: string;
+  numberParticipante: string;
+}
+
+export const CardItems: ICardItems[] = [
+  {
+    image: "images/participantes/character-1.webp",
+    name: "OH IL-NAM",
+    numberParticipante: "001",
+  },
+  {
+    image: "images/participantes/character-2.webp",
+    name: "SANG-WOO",
+    numberParticipante: "002",
+  },
+  {
+    image: "images/participantes/character-3.webp",
+    name: "KANG SA-BYEOK",
+    numberParticipante: "003",
+  },
+];
