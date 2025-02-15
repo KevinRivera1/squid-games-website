17a9da6958e243d5aaf75885c76ea1f9efb77494
Kevin Rivera <shimpo11.kr@gmail.com>
2025-02-15 16:01:31 -0500

    Agregar datos de menú y componentes Slider y SliderContainer

---
+++ src/data/menu/menu.data.ts
@@ -0,0 +1,29 @@
+interface IMenuData {
+  name: string;
+  link: string;
+  Arialabel: string;
+}
+
+export const MenuImtes: IMenuData[] = [
+  {
+    name: "RULES OF THE GAMES",
+    link: "/rules-game",
+    Arialabel: "Rules of the games",
+  },
+  {
+    name: "LIVE",
+    link: "/live",
+    Arialabel: "Live",
+  },
+  {
+    name: "PLAY",
+    link: "/play",
+    Arialabel: "Play",
+  },
+  {
+    name: "Die",
+    link: "/die",
+    Arialabel: "Die",
+  },
+  
+];
