450df831a38f935144f27987ec1ce79ab40d6f98
Kevin Rivera <shimpo11.kr@gmail.com>
2025-02-24 16:52:40 -0500

    refactor: Ajustar la indentación y el formato en varios archivos de configuración y datos
    Nota: Configuracion de Biome para linter

--- src/data/menu/menu.data.ts
+++ src/data/menu/menu.data.ts
@@ -1,29 +1,28 @@
 interface IMenuData {
-  name: string;
-  link: string;
-  Arialabel: string;
+	name: string;
+	link: string;
+	Arialabel: string;
 }
 
 export const MenuImtes: IMenuData[] = [
-  {
-    name: "RULES OF THE GAMES",
-    link: "/rules-game",
-    Arialabel: "Rules of the games",
-  },
-  {
-    name: "LIVE",
-    link: "/live",
-    Arialabel: "Live",
-  },
-  {
-    name: "PLAY",
-    link: "/play",
-    Arialabel: "Play",
-  },
-  {
-    name: "Die",
-    link: "/die",
-    Arialabel: "Die",
-  },
-  
+	{
+		name: "RULES OF THE GAMES",
+		link: "/rules-game",
+		Arialabel: "Rules of the games",
+	},
+	{
+		name: "LIVE",
+		link: "/live",
+		Arialabel: "Live",
+	},
+	{
+		name: "PLAY",
+		link: "/play",
+		Arialabel: "Play",
+	},
+	{
+		name: "Die",
+		link: "/die",
+		Arialabel: "Die",
+	},
 ];
